2991b496e4d4f01ca2a6cce88325736eb08db35d
Rohit <rohitbulusu@gmail.com>
2021-05-01 13:39:10 -0400

    added basic file structure

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+function NavBar() {
+    return (
+        <div>
+            <h1 className="test">Hello World</h1>
+        </div>
+    );
+}
+
+export default NavBar;

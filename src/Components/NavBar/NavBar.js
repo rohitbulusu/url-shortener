4e7874b06cd3f62e4081e82b38285389f1896443
Rohit <rohitbulusu@gmail.com>
2021-05-10 02:35:31 -0400

    fixed problems with eslint and prettier config

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,12 +1,11 @@
 import React from "react";
 
-
 function NavBar() {
-    return (
-        <div>
-            <h1 className="test">Hello World</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="test">Hello World</h1>
+    </div>
+  );
 }
 
 export default NavBar;
